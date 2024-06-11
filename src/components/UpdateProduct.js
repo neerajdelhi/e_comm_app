0d2d652d1c8c8b7c445f1741e0dd255f156f7885
Neeraj sharma <Neeraj@gmail.com>
2024-06-11 19:25:22 +0530

    update react code changes

--- src/components/UpdateProduct.js
+++ src/components/UpdateProduct.js
@@ -1,26 +1,39 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const UpdateProduct = ()=>{
     const [name, setName] = useState("");
     const [price, setPrice] = useState("");
     const [category, setCategory] = useState("");
     const [company, setCompany] = useState("");
-    const [error, setError] = useState(false);
-    const navigate = useNavigate();
+    const params = useParams();
 
-    const handleProduct = async ()=>{
-        console.info(name, price, category, company);
+    useEffect(()=>{
+        getProductDetails();
+    },[]);
+
+    const getProductDetails = async () => {
+        console.warn(params);
+        let result = await fetch(`http://localhost:5000/product/${params.id}`);
+         result = await result.json();
+        setName(result.name);
+        setPrice(result.price);
+        setCategory(result.category);
+        setCompany(result.company);
+    }
+
+    const UpdateProduct = async ()=> {
+        console.warn(name, price, category, company);
     }
 
     return (
-        <div>
-            <h1 className='center'>Add Product</h1>
-            <input type="text"  value={name} onChange={(e)=>setName(e.target.value)} placeholder="type product name" />
-            <input type="text"  value={price} onChange={(e)=>setPrice(e.target.value)} placeholder="type product price"/>
-            <input type="text"  value={category} onChange={(e)=>setCategory(e.target.value)} placeholder="type product category"/>
-            <input type="text"  value={company} onChange={(e)=>setCompany(e.target.value)} placeholder="type product company"/>
-            <center><button onClick={handleProduct}>Add Product</button></center>
+        <div className='product'>
+            <h1>Update Product</h1>
+            <input type="text"  value={name} onChange={(e)=>setName(e.target.value)} placeholder="type product name" className='inputBox' />
+            <input type="text"  value={price} onChange={(e)=>setPrice(e.target.value)} placeholder="type product price" className='inputBox'/>
+            <input type="text"  value={category} onChange={(e)=>setCategory(e.target.value)} placeholder="type product category" className='inputBox'/>
+            <input type="text"  value={company} onChange={(e)=>setCompany(e.target.value)} placeholder="type product company" className='inputBox'/>
+            <center><button onClick={UpdateProduct} className='appButton'>Update Product</button></center>
         </div>
     )
 }
